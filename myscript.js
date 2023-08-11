040c9d7e06cae73e0e5e31696c9e7f8da5fab188
Cashton Holbert <cashtoncoleholbert@gmail.com>
2023-08-11 15:35:35 -0700

    evaluate functionality

--- myscript.js
+++ myscript.js
@@ -1,3 +1,7 @@
+const ACCEPTED_OPERATORS = ["(", ")", "+", "-", "*", "/", "^"];
+const ACCEPTED_CHAR = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const ACCEPTED_ACTIONS = ["=", "C", "DEL"];
+
 
 function add(a, b) {return a+b};
 
@@ -7,6 +11,8 @@ function mul(a, b) {return a*b};
 
 function div(a, b) {return a/b};
 
+function pow(a, b) {return Math.pow(a, b)};
+
 let numA;
 let numB;
 let operator;
@@ -15,12 +21,41 @@ function operate(operator, numA, numB) {
 
     switch(operator){
         case "+":
-            return add(numA, numB);
+            return add(numA * 1, numB * 1); // * 1 to convert to string... may not work as intended...
         case "-":
-            return sub(numA, numB);
+            return sub(numA * 1, numB * 1);
         case "*":
-            return mul(numA, numB);
+            return mul(numA * 1, numB * 1);
         case "/":
-            return div(numA, numB);
+            return div(numA * 1, numB * 1);
+        case "^":
+            return pow(numA * 1, numB * 1);
     }
 }
+
+function evalExpression(expression)
+{
+
+
+    
+
+
+}
+
+let inputString = "";
+let screenText = document.querySelector('.screen');
+const buttons = document.querySelectorAll('.button');
+console.log(buttons);
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        inputString = inputString.concat(String(button.innerHTML));
+
+        // check for operation
+        
+        screenText.textContent = inputString;
+    });
+});
+
+
+
+
