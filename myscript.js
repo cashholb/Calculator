f824a0ef245b66b4dd0723336daa9f695e1f641a
Cashton Holbert <cashtoncoleholbert@gmail.com>
2023-08-14 22:15:10 -0700

    finished styled calculator

--- myscript.js
+++ myscript.js
@@ -1,7 +1,6 @@
-const ACCEPTED_OPERATORS = ["(", ")", "+", "-", "*", "/", "^"];
-const ACCEPTED_CHAR = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const ACCEPTED_OPERATORS = ["(", ")", "+", "-", "x", "/", "^"];
+const ACCEPTED_CHAR = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const ACCEPTED_ACTIONS = ["=", "C", "DEL"];
-
 
 function add(a, b) {return a+b};
 
@@ -13,9 +12,7 @@ function div(a, b) {return a/b};
 
 function pow(a, b) {return Math.pow(a, b)};
 
-let numA;
-let numB;
-let operator;
+let operator = "";
 
 function operate(operator, numA, numB) {
 
@@ -24,7 +21,7 @@ function operate(operator, numA, numB) {
             return add(numA * 1, numB * 1); // * 1 to convert to string... may not work as intended...
         case "-":
             return sub(numA * 1, numB * 1);
-        case "*":
+        case "x":
             return mul(numA * 1, numB * 1);
         case "/":
             return div(numA * 1, numB * 1);
@@ -32,30 +29,98 @@ function operate(operator, numA, numB) {
             return pow(numA * 1, numB * 1);
     }
 }
+let havePreviousVal = false;
+let valPrev = 0
+let valCurr = 0
 
-function evalExpression(expression)
-{
-
-
-    
-
-
-}
-
-let inputString = "";
-let screenText = document.querySelector('.screen');
+let screenText = document.querySelector('.screenCurr');
+let prevScreenText = document.querySelector('.screenPrev');
 const buttons = document.querySelectorAll('.button');
-console.log(buttons);
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        inputString = inputString.concat(String(button.innerHTML));
 
-        // check for operation
+        input = String(button.innerHTML);
+        if(ACCEPTED_ACTIONS.includes(input)){
+            switch(input){
+                case "=":
+                    if(operator != "" && havePreviousVal){
+                        if(valCurr == 0 && operator == "/")
+                        {
+                            valCurr = 0;
+                            screenText.textContent = "good try ;)";
+                            operator = "";
+                            prevScreenText.textContent = "";
+                            
+                        }else{
+                            prevScreenText.textContent = `${prevScreenText.textContent} ${valCurr} =`;
+                            valCurr = operate(operator, Number(valPrev), Number(valCurr));
+                            screenText.textContent = String(valCurr);
+                            operator = "";
+                            
+                        }
+                        valPrev = 0;
+                        havePreviousVal = false;
+                    }
+                    
+                    break;
+                case "DEL":
+                    if (valCurr.toString().length < 2) {
+                        valCurr = 0;
+                        prevScreenText.textContent = "";
+
+                    }else{
+
+                        input = screenText.textContent.substring(0,screenText.textContent.length-1);
+                        console.log(input);
+                        valCurr = parseInt(input);
+                    }
+                    screenText.textContent = String(valCurr);
+                    break;
+                case "C":
+                    input = "0";
+                    valCurr = 0;
+                    valPrev = 0;
+                    havePrevious = false;
+                    operator = "";
+                    prevScreenText.textContent = "";
+                    screenText.textContent = String(valCurr);
+                    break;
+            }
         
-        screenText.textContent = inputString;
+
+        }else if(ACCEPTED_CHAR.includes(input)){
+
+            if(operator != "" && havePreviousVal == false)
+            {
+                valPrev = valCurr;
+                screenText.textContent = `${input}`;
+                valCurr = parseFloat(`${screenText.textContent}`);
+                havePreviousVal = true;
+                prevScreenText.textContent = `${valPrev} ${operator}`;
+            }else{
+                if(!Number.isInteger(Number(screenText.textContent)) || screenText.textContent == "0" || screenText.textContent == "good try ;)")
+                {
+                    screenText.textContent = "";
+                }
+
+                screenText.textContent = `${screenText.textContent}${input}`;
+                valCurr = parseFloat(`${screenText.textContent}`);
+            }
+
+        }else if (input == "."){
+            console.log(valCurr);
+            if(!screenText.textContent.includes("."))
+            {
+                screenText.textContent = `${screenText.textContent}${input}`;
+                valCurr = parseFloat(`${screenText.textContent}`);
+            }
+
+        }else if(ACCEPTED_OPERATORS.includes(input)){
+
+            operator = String(input);
+            console.log(operator);
+        }
+        
     });
 });
-
-
-
 
